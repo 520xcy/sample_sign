a012941f1d6e09f88ecb182747269c047db9d5d2
TsiayauHsiang <greatcc@vip.qq.com>
2020-10-25 22:34:42 +0800

    修改：去除点击a元素target属性，日志换行

--- Model/sign.py
+++ Model/sign.py
@@ -13,60 +13,62 @@ import pickle
 import os
 
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.common.exceptions import TimeoutException,WebDriverException,NoSuchElementException,NoSuchWindowException
+from selenium.common.exceptions import TimeoutException, WebDriverException, NoSuchElementException, NoSuchWindowException
+
 
 class SIGN:
-    def __init__(self,chrome_driver='',options='',sitename='',login='',afterlogin='',signurl='',xpath=[]):
+    def __init__(self, chrome_driver=[], options='', sitename='', login='', afterlogin='', signurl='', xpath=[]):
         self.SITENAME = sitename
         self.LOGIN = login
         self.AFTERLOGIN = afterlogin
         self.SIGNURL = signurl
         self.XPATH = xpath
         self.BASE_PATH = os.getcwd()
-        self.PATH = self.setDir(self.BASE_PATH+'/Sites',self.SITENAME)
+        self.PATH = self.setDir(self.BASE_PATH+'/Sites', self.SITENAME)
 
         chromeOptions = webdriver.ChromeOptions()
-        #设置编码格式
-        chromeOptions.add_argument('lang=zh_CN.UTF-8')
-        #模拟移动设备   (移动版网站的反爬虫的能力比较弱)   模拟iPhone6
-        # chromeOptions.add_argument('user-agent="Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1"') 
-        #取消沙盒模式，解决DevToolsActivePort文件不存在的报错
+        # 设置编码格式
+        # chromeOptions.add_argument('lang=zh_CN.UTF-8')
+        # 模拟移动设备   (移动版网站的反爬虫的能力比较弱)   模拟iPhone6
+        # chromeOptions.add_argument('user-agent="Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1"')
+        # 取消沙盒模式，解决DevToolsActivePort文件不存在的报错
         # chromeOptions.add_argument('--no-sandbox')
-        #谷歌文档提到需要加上这个属性来规避bug
-        chromeOptions.add_argument('--disable-gpu')  
-        #克服有限的资源问题  【但是用于Linux系统】
+        # 谷歌文档提到需要加上这个属性来规避bug
+        # chromeOptions.add_argument('--disable-gpu')
+        # 克服有限的资源问题  【但是用于Linux系统】
         # chromeOptions.add_argument('--disable-dev-shm-usage')
-        #隐藏滚动条, 应对一些特殊页面  
-        # chromeOptions.add_argument('--hide-scrollbars') 
+        #隐藏滚动条, 应对一些特殊页面
+        # chromeOptions.add_argument('--hide-scrollbars')
         #不加载图片, 提升速度
-        chromeOptions.add_argument('blink-settings=imagesEnabled=false')
+        # chromeOptions.add_argument('blink-settings=imagesEnabled=false')
         #浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
         # chromeOptions.add_argument('--headless')
-        if options: chromeOptions.add_argument(options)
+        if options:
+            for option in options:
+                chromeOptions.add_argument(option)
         if chrome_driver:
-            self.BROWER = webdriver.Chrome(executable_path = chrome_driver,options=chromeOptions)
+            self.BROWER = webdriver.Chrome(executable_path=chrome_driver, options=chromeOptions)
         else:
             self.BROWER = webdriver.Chrome(options=chromeOptions)
         WebDriverWait(self.BROWER, 10)
         self.BROWER.maximize_window()
         self.BROWER.implicitly_wait(6)
 
-    def setDir(self,basepath, dir):
+    def setDir(self, basepath, dir):
         newdir = basepath+'/'+dir
         if dir not in os.listdir(basepath):
             os.makedirs(newdir)
         return newdir
 
-    def writefile(self,fileURI, str):
+    def writefile(self, fileURI, str):
         with open(fileURI, 'a', encoding='UTF-8') as w:
-            w.write(str)
+            w.write(str+"\n")
 
     def log(self, str):
         print(str)
-        str = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))+': '+str
-        self.writefile(self.PATH+'/'+self.SITENAME+'.log',str)
+        str = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))+': '+str
+        self.writefile(self.PATH+'/'+self.SITENAME+'.log', str)
 
     def geterror(self, e):
         self.log(e)
@@ -90,7 +92,6 @@ class SIGN:
                 outputPath.close()
                 return cookies
 
-
     def readCookies(self):
         # if hava cookies file ,use it
         # if not , getTaobaoCookies()
@@ -100,7 +101,6 @@ class SIGN:
         else:
             Cookies = self.getCookies()
         return Cookies
-
 
     def run(self):
         Cookies = self.readCookies()
@@ -117,9 +117,13 @@ class SIGN:
         try:
             self.BROWER.get(self.SIGNURL)
             for path in self.XPATH:
-                self.BROWER.find_element_by_xpath(path).click()
+                # 防止a标签新窗口打开，简单实现，复杂情况还是需要switch
+                xpath = self.BROWER.find_element_by_xpath(path)
+                if xpath.tag_name == 'a':
+                    self.BROWER.execute_script("arguments[0].removeAttribute(arguments[1]);", xpath, "target")
+                xpath.click()
             pass
-        except (NoSuchElementException, TimeoutException, WebDriverException,NoSuchWindowException) as e:
+        except (NoSuchElementException, TimeoutException, WebDriverException, NoSuchWindowException) as e:
             self.geterror(str(e))
         except:
             self.geterror('未知错误')
@@ -127,4 +131,3 @@ class SIGN:
             self.log('签到成功')
         finally:
             self.BROWER.quit()
-
